e53bd457fc38c57e7d646e48c5093189870a56f3
Dominic Crosby <dominiccrosby@Dominics-MacBook.local>
2019-01-16 16:38:30 -0800

    Added all files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import firebase from 'firebase';
+import AppContainer from './src/Router';
+
+export default class App extends Component {
+  componentWillMount() {
+    // Insert Firebase here
+  }
+
+  render() {
+    return <AppContainer />;
+  }
+}
